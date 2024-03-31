fc24df4a7380e91fe5bef6b8f8cdd1d8b59c67a1
Deus lo Vult <moises.barrera.dev@gmail.com>
2024-03-31 02:30:17 -0300

    agregado el skeleton

--- src/components/Products/ProductSkeleton.tsx
+++ src/components/Products/ProductSkeleton.tsx
@@ -1,6 +1,14 @@
 const ProductSkeleton = () => {
   return (
-    <div>ProductSkeleton</div>
-  )
-}
-export default ProductSkeleton
+    <div className="relative animate-pulse">
+      <div className="bg-gray-200 rounded-md aspect-w-1 w-full aspect-h-1 lg:h-80 overflow-hidden lg:aspect-none">
+        <div className="bg-gray-200 size-full" />
+      </div>
+      <div className="flex flex-col gap-2 mt-4" >
+        <div className="bg-gray-200" />
+      </div>
+      
+    </div>
+  );
+};
+export default ProductSkeleton;
