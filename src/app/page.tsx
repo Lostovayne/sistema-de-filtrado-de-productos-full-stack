25f8cb80a52182e80bd2f38ac05a95c5a6674043
Deus lo Vult <moises.barrera.dev@gmail.com>
2024-04-01 00:45:26 -0300

    agregado el filtro de color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,32 @@ import axios from "axios";
 
 import ProductCard from "@/components/Products/Product";
 import ProductSkeleton from "@/components/Products/ProductSkeleton";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 const SORT_OPTIONS = [
-  {
-    name: "None",
-    value: "none",
-  },
-  {
-    name: "Price: Low to High",
-    value: "price-asc",
-  },
-  {
-    name: "Price: High to Low",
-    value: "price-desc",
-  },
+  { name: "None", value: "none" },
+  { name: "Price: Low to High", value: "price-asc" },
+  { name: "Price: High to Low", value: "price-desc" },
 ] as const;
+
+const COLOR_FILTERS = {
+  id: "color",
+  name: "Color",
+  options: [
+    { value: "white", label: "White" },
+    { value: "beige", label: "Beige" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+    { value: "purple", label: "Purple" },
+  ] as const,
+};
+
+const SUBCATEGORIES = [
+  { name: "T-Shirts", selected: true, href: "#" },
+  { name: "Hoodies", selected: false, href: "#" },
+  { name: "Sweatshirts", selected: false, href: "#" },
+  { name: "Accessories", selected: false, href: "#" },
+];
 
 export default function Home() {
   const [filter, setFilter] = useState({
@@ -46,8 +57,6 @@ export default function Home() {
       return data;
     },
   });
-
-  console.log(products);
 
   return (
     <main className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
@@ -86,7 +95,42 @@ export default function Home() {
       <section className="pt-6 pb-24">
         <div className="gap-x-8 gap-y-10 grid grid-cols-1 lg:grid-cols-4">
           {/* Filters */}
-          <div></div>
+          <div className="lg:block hidden">
+            <ul className="space-y-4 border-gray-200 pb-6 border-b font-medium text-gray-900 text-sm">
+              {SUBCATEGORIES.map((category) => (
+                <li key={category.name}>
+                  <button disabled={!category.selected} className="disabled:opacity-60 disabled:cursor-not-allowed">
+                    {category.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+
+            <Accordion type="multiple" className="animate-none">
+              {/* Color filter */}
+              <AccordionItem value="color">
+                <AccordionTrigger className="py-3 text-gray-400 text-sm hover:text-gray-500 hover:no-underline">
+                  <span className="font-medium text-gray-900">Color</span>
+                </AccordionTrigger>
+                <AccordionContent className="pt-6 animate-none">
+                  <ul className="space-y-4">
+                    {COLOR_FILTERS.options.map((option, optionIdx) => (
+                      <li key={option.value} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          id={`color-${optionIdx}`}
+                          className="border-gray-300 text-slate-900 size-4 focus:ring-slate-950 accent-slate-700"
+                        />
+                        <label htmlFor={`color-${optionIdx}`} className="ml-3 text-gray-600 text-sm">
+                          {option.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
 
           {/* Products grid */}
           <ul className="gap-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:col-span-3">
--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -1,24 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
 import type { Product } from "@/db";
 
-const ProductCard = ({ product }: { product: Product }) => {
+/**
+ * Renders a product card with the provided product data.
+ * @param {Object} props - The properties of the product.
+ * @param {Product} props.product - The product data to be rendered.
+ * @returns {JSX.Element} The rendered product card.
+ */
+const ProductCard = ({ product }: { product: Product }): JSX.Element => {
   return (
-    <div className="relative group">
-      <div className="bg-gray-200 group-hover:opacity-75 rounded-md w-full lg:h-80 overflow-hidden aspect-square lg:aspect-none">
-        <img src={product.imageId} alt="product image" className="w-full h-full object-center object-contain" />
-      </div>
+    <article className="relative group">
+      <figure className="bg-gray-200 group-hover:opacity-75 rounded-md w-full lg:h-80 overflow-hidden aspect-square lg:aspect-none">
+        <img
+          src={product.imageId}
+          alt={`Image of ${product.name}`}
+          className="w-full h-full object-center object-contain"
+        />
+        <figcaption className="sr-only">Image of {product.name}</figcaption>
+      </figure>
 
-      <div className="flex justify-between mt-4">
-        <div>
+      <section className="flex justify-between mt-4">
+        <header>
           <h3 className="text-gray-700 text-sm">{product.name}</h3>
           <p className="mt-1 text-gray-500 text-sm">
             Size {product.size.toUpperCase()}, {product.color}
           </p>
-        </div>
+        </header>
 
         <p className="font-medium text-gray-900 text-sm">{product.price}</p>
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 export default ProductCard;
